557aa74246f26ca956dfe525dd02921a80c2b5b5
zachthecoder14 <82949634+zachthecoder14@users.noreply.github.com>
2021-04-21 20:28:56 +0200

    Update store.py

--- store.py
+++ store.py
@@ -1,13 +1,15 @@
 # Pi-Ware main UI by ZachTheCoder
-#test
 
 import tkinter as tk
 import os
 
-os.system("sudo python3 update.py")
+os.system("cd /home/pi/pi-ware")
+os.system("git pull")
 
 window = tk.Tk()
 window.resizable(0, 0)
 window.geometry("320x500")
 window.title("Pi-Ware")
 
+
+window.mainloop()
