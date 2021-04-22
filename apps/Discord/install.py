8712434f94d13601682126811979c5c977f36465
Painadath <74921727+Painadath@users.noreply.github.com>
2021-04-22 18:54:05 +0530

    change to the oneline script for speed

--- apps/Discord/install.py
+++ apps/Discord/install.py
@@ -2,6 +2,4 @@
 
 import os
 
-os.system("wget https://raw.githubusercontent.com/Botspot/pi-apps/master/apps/Discord/install-32 | bash")
-os.system("sudo /home/pi/install-32")
-os.system("sudo rm /home/pi/install-32")
+os.system("wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/apps/Discord/install-32 | bash")
