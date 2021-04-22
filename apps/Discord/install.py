2605f364fdd7c4802c60476eee0ea1adc7584488
zachthecoder14 <82949634+zachthecoder14@users.noreply.github.com>
2021-04-22 15:07:01 +0200

    Add install and uninstall scripts (for testing purposes)

---
+++ apps/Discord/install.py
@@ -0,0 +1,7 @@
+# Discord install script by ZachTheCoder
+
+import os
+
+os.system("wget https://raw.githubusercontent.com/Botspot/pi-apps/master/apps/Discord/install-32 | bash")
+os.system("sudo /home/pi/install-32")
+os.system("sudo rm /home/pi/install-32")
---
+++ apps/Discord/uninstall.py
@@ -0,0 +1,7 @@
+# Discord uninstall script by ZachTheCoder
+
+import os
+
+os.system("wget https://raw.githubusercontent.com/Botspot/pi-apps/master/apps/Discord/uninstall | bash")
+os.system("sudo /home/pi/uninstall")
+os.system("sudo rm /home/pi/uninstall")
