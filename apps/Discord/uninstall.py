e5471f6393b44aaf736b35ec254324aff3c38151
Painadath <74921727+Painadath@users.noreply.github.com>
2021-04-22 18:55:09 +0530

    update unisntall to one line script for speed

--- apps/Discord/uninstall.py
+++ apps/Discord/uninstall.py
@@ -2,6 +2,5 @@
 
 import os
 
-os.system("wget https://raw.githubusercontent.com/Botspot/pi-apps/master/apps/Discord/uninstall | bash")
-os.system("sudo /home/pi/uninstall")
-os.system("sudo rm /home/pi/uninstall")
+os.system("wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/apps/Discord/uninstall | bash")
+
