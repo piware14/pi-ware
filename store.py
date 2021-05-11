8f81d07ca4ba6f084929c1383179915e8bd3922c
Alvarito050506 <donfrutosgomez@gmail.com>
2021-05-11 16:49:04 -0300

    Refactoring a bit, part 1

--- store.py
+++ store.py
@@ -1,82 +1,94 @@
+#!/usr/bin/env python3
 # Pi-Ware main UI
 
 import tkinter as tk
 import os
+from os.path import dirname, realpath
 from functools import partial
 
-window = tk.Tk()
-window.resizable(0, 0)
-window.geometry("320x500")
-window.title("Pi-Ware")
+pw_prefix = dirname(dirname(realpath(__file__)));
+apps_dir = f"{pw_prefix}/share/pi-ware/apps/";
 
-def show_desc(app):
-    global install_script,uninstall_script,desc_win
-    desc_win = tk.Toplevel(window)
-    desc_win.title("Pi-Ware")
-    desc_win.geometry("320x500")
-    window.withdraw()
-    desc = open(f"/home/pi/pi-ware/apps/{app}/description.txt", "r")
-    desc_contents = desc.read()
-    app_desc = tk.Label(desc_win,
-        text=desc_contents,
-        font="Arial 9")
-    app_desc.pack()
-    install = tk.Button(desc_win,
-        text="INSTALL",
-        font="Arial 11 bold",
-        width=200,
-        bg="darkblue",
-        fg="white",
-        command=install_app)
-    install.pack()
-    uninstall = tk.Button(desc_win,
-        text="UNINSTALL",
-        font="Arial 11 bold",
-        width=200,
-        bg="red",
-        fg="white",
-        command=uninstall_app)
-    uninstall.pack()
-    back_to_menu_button = tk.Button(desc_win,
-    text="BACK",
-    font="Arial 11 bold",
-    width=200,
-    height=2,
-    bg="green",
-    fg="white",
-    command=back_to_menu)
-    back_to_menu_button.pack(side = "bottom")
-    ucommand = "sudo bash /home/pi/pi-ware/apps/%s/uninstall" % app
-    command = "sudo bash /home/pi/pi-ware/apps/%s/install" % app
-    install_script = "lxterminal -e '%s'" % command
-    uninstall_script = "lxterminal -e '%s'" % ucommand
+class WrapLabel(tk.Label):
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, wraplength=315, justify="center", *args, **kwargs)
 
-applist = next(os.walk("/home/pi/pi-ware/apps"))[1]
-print("Current apps:\n")
-for app in applist:
-    print(app)
-    exec(app + """_button =  tk.Button(window,
-            text=app,
+class AppButton(tk.Button):
+    def __init__(self, parent, app, fg=None, command=None, *args, **kwargs):
+        super().__init__(parent,
             font="Arial 11 bold",
             width=200,
-            bg="gray",
-            fg="white",
-            command=partial(show_desc,app))""")
-    exec(app + "_button.pack()")
+            fg=(fg or "white"),
+            command=partial(command, parent, app),
+        *args, **kwargs)
 
-def install_app():
-    global install_script
+def show_desc(window, app):
+    desc_win = tk.Toplevel(window)
+    desc_win.title(f"{app} on Pi-Ware")
+    desc_win.geometry("320x500")
+    desc_win.wm_protocol("WM_DELETE_WINDOW", partial(back_to_menu, window, desc_win))
+    window.withdraw()
+
+    with open(f"{apps_dir}/{app}/description.txt", "r") as desc:
+        desc_contents = desc.read()
+        app_desc = WrapLabel(desc_win,
+            text=desc_contents,
+            font="Arial 9")
+        app_desc.pack()
+
+    install = AppButton(desc_win, app,
+        text="INSTALL",
+        bg="darkblue",
+        command=install_app)
+    uninstall = AppButton(desc_win, app,
+        text="UNINSTALL",
+        bg="red",
+        command=uninstall_app)
+    back = AppButton(desc_win, app,
+        text="BACK",
+        height=2,
+        bg="green",
+        command=partial(back_to_menu, window))
+
+    install.pack()
+    uninstall.pack()
+    back.pack(side = "bottom")
+
+def install_app(parent, app):
+    command = f"sudo bash {apps_dir}/{app}/install"
+    install_script = f"x-terminal-emulator -e '{command}'"
     os.system(install_script)
     
-def uninstall_app():
-    global uninstall_script
+def uninstall_app(parent, app):
+    ucommand = f"sudo bash {apps_dir}/{app}/uninstall"
+    uninstall_script = f"x-terminal-emulator -e '{ucommand}'"
     os.system(uninstall_script)
     
-def back_to_menu():
+def back_to_menu(window, parent, app=None):
+    parent.destroy()
     window.deiconify()
-    desc_win.destroy()
+
+def main():
+    window = tk.Tk()
+    window.resizable(0, 0)
+    window.geometry("320x500")
     window.title("Pi-Ware")
-    
-blank_line = tk.Label(text="")
-blank_line.pack()
-window.mainloop()
+
+    blank_line = tk.Label(text="")
+    blank_line.pack()
+
+    applist = next(os.walk(apps_dir))[1]
+    print("Current apps:\n")
+    for app in applist:
+        print(app)
+        button = AppButton(window, app, text=app, bg="gray", command=show_desc)
+        button.pack()
+
+    window.wm_protocol("WM_DELETE_WINDOW", window.destroy)
+    try:
+        window.mainloop()
+    except KeyboardInterrupt:
+        window.destroy()
+
+if __name__ == "__main__":
+    main()
