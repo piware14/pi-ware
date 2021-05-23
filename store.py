bb4eca277851b10d40461f6fd81c07bc1f490e26
zachthecoder14 <82949634+zachthecoder14@users.noreply.github.com>
2021-05-23 16:05:52 +0200

    Update store.py

--- store.py
+++ store.py
@@ -17,7 +17,39 @@ def show_desc(app):
     desc_win.title("Pi-Ware")
     desc_win.geometry("320x500")
     window.withdraw()
-    desc = open(f"/home/sajo/pi-ware/apps/{app}/description.txt", "r")
+    desc = open(f"/home/pi/pi-ware/apps/{app}/description.txt", "r")
+    desc_contents = desc.read()
+    app_desc = tk.Label(desc_win,
+        text=desc_contents,
+        font="Arial 9")
+    app_desc.pack()
+    install = tk.Button(desc_win,
+        text="INSTALL",
+        font="Arial 11 bold",
+        width=200,
+        bg="darkblue",
+        fg="white",
+        command=install_app)
+    install.pack()
+    uninstall = tk.Button(desc_win,
+import tkinter as tk
+import os
+from functools import partial
+import string
+
+window = tk.Tk()
+window.resizable(0, 0)
+window.geometry("320x500")
+window.title("Pi-Ware")
+
+
+def show_desc(app):
+    global install_script,uninstall_script,desc_win
+    desc_win = tk.Toplevel(window)
+    desc_win.title("Pi-Ware")
+    desc_win.geometry("320x500")
+    window.withdraw()
+    desc = open(f"/home/pi/pi-ware/apps/{app}/description.txt", "r")
     desc_contents = desc.read()
     app_desc = tk.Label(desc_win,
         text=desc_contents,
@@ -48,12 +80,12 @@ def show_desc(app):
     fg="white",
     command=back_to_menu)
     back_to_menu_button.pack(side = "bottom")
-    ucommand = "sudo bash /home/sajo/pi-ware/apps/%s/uninstall" % app
-    command = "sudo bash /home/sajo/pi-ware/apps/%s/install" % app
+    ucommand = "sudo bash /home/pi/pi-ware/apps/%s/uninstall" % app
+    command = "sudo bash /home/pi/pi-ware/apps/%s/install" % app
     install_script = "lxterminal -e '%s'" % command
     uninstall_script = "lxterminal -e '%s'" % ucommand
 
-ap = next(os.walk("/home/sajo/pi-ware/apps"))[1]
+ap = next(os.walk("/home/pi/pi-ware/apps"))[1]
 applist = sorted(ap)
 print("Current apps:\n")
 for app in applist:
