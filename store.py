750449711d59fc09358847346f00267ed0ef858c
zachthecoder14 <82949634+zachthecoder14@users.noreply.github.com>
2021-05-23 18:12:04 +0200

    Fix app not running

--- store.py
+++ store.py
@@ -3,45 +3,11 @@
 import tkinter as tk
 import os
 from functools import partial
-import string
 
 window = tk.Tk()
 window.resizable(0, 0)
 window.geometry("320x500")
 window.title("Pi-Ware")
-
-
-def show_desc(app):
-    global install_script,uninstall_script,desc_win
-    desc_win = tk.Toplevel(window)
-    desc_win.title("Pi-Ware")
-    desc_win.geometry("320x500")
-    window.withdraw()
-    desc = open(f"/home/pi/pi-ware/apps/{app}/description.txt", "r")
-    desc_contents = desc.read()
-    app_desc = tk.Label(desc_win,
-        text=desc_contents,
-        font="Arial 9")
-    app_desc.pack()
-    install = tk.Button(desc_win,
-        text="INSTALL",
-        font="Arial 11 bold",
-        width=200,
-        bg="darkblue",
-        fg="white",
-        command=install_app)
-    install.pack()
-    uninstall = tk.Button(desc_win,
-import tkinter as tk
-import os
-from functools import partial
-import string
-
-window = tk.Tk()
-window.resizable(0, 0)
-window.geometry("320x500")
-window.title("Pi-Ware")
-
 
 def show_desc(app):
     global install_script,uninstall_script,desc_win
@@ -85,39 +51,32 @@ def show_desc(app):
     install_script = "lxterminal -e '%s'" % command
     uninstall_script = "lxterminal -e '%s'" % ucommand
 
-ap = next(os.walk("/home/pi/pi-ware/apps"))[1]
-applist = sorted(ap)
+applist = next(os.walk("/home/pi/pi-ware/apps"))[1]
 print("Current apps:\n")
 for app in applist:
     print(app)
-    appb = ""
-    for a in app:
-        if(a == " "):
-            appb += "_"
-        else:
-            appb += a
-    exec(appb + """_button =  tk.Button(window,
+    exec(app + """_button =  tk.Button(window,
             text=app,
             font="Arial 11 bold",
             width=200,
             bg="gray",
             fg="white",
             command=partial(show_desc,app))""")
-    exec(appb + "_button.pack()")
+    exec(app + "_button.pack()")
 
 def install_app():
     global install_script
     os.system(install_script)
-
+    
 def uninstall_app():
     global uninstall_script
     os.system(uninstall_script)
-
+    
 def back_to_menu():
     window.deiconify()
     desc_win.destroy()
     window.title("Pi-Ware")
-
+    
 blank_line = tk.Label(text="")
 blank_line.pack()
 window.mainloop()
