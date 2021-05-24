660099409cefe335205a4fd4a9679ad22c824f0e
zachthecoder14 <82949634+zachthecoder14@users.noreply.github.com>
2021-05-24 15:21:41 +0200

    STORE.PY MAJOR UPDATES

--- store.py
+++ store.py
@@ -27,6 +27,75 @@ def show_desc(app):
     install = tk.Button(desc_win,
         text="INSTALL",
         font="Arial 11 bold",
+# Pi-Ware main UI
+
+import tkinter as tk
+import os
+from functools import partial
+import getpass
+class ScrolledFrame(tk.Frame):
+    def __init__(self, parent, vertical=True, horizontal=False):
+        super().__init__(parent)
+
+        # canvas for inner frame
+        self._canvas = tk.Canvas(self)
+        self._canvas.grid(row=0, column=0, sticky="news") # changed
+
+        # create right scrollbar and connect to canvas Y
+        self._vertical_bar = tk.Scrollbar(self, orient="vertical", command=self._canvas.yview)
+        if vertical:
+            self._vertical_bar.grid(row=0, column=1, sticky="ns")
+        self._canvas.configure(yscrollcommand=self._vertical_bar.set)
+
+        # create bottom scrollbar and connect to canvas X
+        self._horizontal_bar = tk.Scrollbar(self, orient="horizontal", command=self._canvas.xview)
+        if horizontal:
+            self._horizontal_bar.grid(row=1, column=0, sticky="we")
+        self._canvas.configure(xscrollcommand=self._horizontal_bar.set)
+
+        # inner frame for widgets
+        self.inner = tk.Frame(self._canvas, bg="red")
+        self._window = self._canvas.create_window((0, 0), window=self.inner, anchor="nw")
+
+        # autoresize inner frame
+        self.columnconfigure(0, weight=1) # changed
+        self.rowconfigure(0, weight=1) # changed
+
+        # resize when configure changed
+        self.inner.bind("<Configure>", self.resize)
+        self._canvas.bind("<Configure>", self.frame_width)
+
+    def frame_width(self, event):
+        # resize inner frame to canvas size
+        canvas_width = event.width
+        self._canvas.itemconfig(self._window, width = canvas_width)
+
+    def resize(self, event=None):
+        self._canvas.configure(scrollregion=self._canvas.bbox('all'))
+window = tk.Tk()
+window.resizable(0, 0)
+window.geometry("320x500")
+window.title("Pi-Ware")
+frame = ScrolledFrame(window)
+frame.pack(expand=True, fill="both")
+
+computer_username = getpass.getuser()
+
+def show_desc(app):
+    global install_script, uninstall_script, desc_win
+    desc_win = tk.Toplevel(window)
+    desc_win.title("Pi-Ware")
+    desc_win.geometry("320x500")
+    window.withdraw()
+    desc = open(f"/home/{computer_username}/pi-ware/apps/{app}/description.txt", "r")
+    desc_contents = desc.read()
+    app_desc = tk.Label(desc_win,
+        text=desc_contents,
+        font="Arial 9")
+    app_desc.pack()
+    install = tk.Button(desc_win,
+        text="INSTALL",
+        font="Arial 11 bold",
         width=200,
         bg="darkblue",
         fg="white",
@@ -49,23 +118,32 @@ def show_desc(app):
     fg="white",
     command=back_to_menu)
     back_to_menu_button.pack(side = "bottom")
-    ucommand = "sudo bash /home/{username}/pi-ware/apps/%s/uninstall" % app
-    command = "sudo bash /home/{username}/pi-ware/apps/%s/install" % app
+    ucommand = "sudo bash /home/{computer_username}/pi-ware/apps/%s/uninstall" % app
+    command = "sudo bash /home/{computer_username}/pi-ware/apps/%s/install" % app
     install_script = "lxterminal -e '%s'" % command
     uninstall_script = "lxterminal -e '%s'" % ucommand
-
-applist = next(os.walk(f"/home/{username}/pi-ware/apps"))[1]
+def back_to_menu(window, parent, app=None):
+    parent.destroy()
+    window.deiconify()
+ap = next(os.walk(f"/home/{computer_username}/pi-ware/apps"))[1]
+applist = sorted(ap)
 print("Current apps:\n")
 for app in applist:
     print(app)
-    exec(app + """_button =  tk.Button(window,
+    appb = ""
+    for a in app:
+        if(a == " "):
+            appb += "_"
+        else:
+            appb += a
+    exec(appb + """_button =  tk.Button(frame.inner,
             text=app,
             font="Arial 11 bold",
             width=200,
             bg="gray",
             fg="white",
             command=partial(show_desc,app))""")
-    exec(app + "_button.pack()")
+    exec(appb + "_button.pack()")
 
 def install_app():
     global install_script
