93e31b6792015b0e31b41734fe72f54214607fd4
oxmc <67136658+oxmc@users.noreply.github.com>
2021-05-30 06:17:19 -0700

    Update store.py

--- store.py
+++ store.py
@@ -45,13 +45,13 @@ class ScrolledFrame(tk.Frame):
         self._canvas.configure(scrollregion=self._canvas.bbox('all'))
 window = tk.Tk()
 #Set window icon
-window.tk.call('wm', 'iconphoto', window._w, tk.PhotoImage(file=f'/home/{username}/pi-ware/icons/logo.png')
+#window.tk.call('wm', 'iconphoto', window._w, tk.PhotoImage(file=f'/home/{username}/pi-ware/icons/logo.png')
 #Set top icon
 #img = ImageTk.PhotoImage(Image.open(f'/home/{username}/pi-ware/icons/logo.png'))
 #panel = Label(window, image = img)
 #panel.pack(side = "bottom", fill = "both", expand = "yes")
 #Main
-#window.resizable(0, 0)
+window.resizable(0, 0)
 window.geometry("320x500")
 window.title("Pi-Ware")
 frame = ScrolledFrame(window)
