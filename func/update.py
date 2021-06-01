225f4cd5299d6a09dc96c7ba101afe5c74ffe55c
oxmc <67136658+oxmc@users.noreply.github.com>
2021-05-31 23:27:44 -0700

    Clean up code

--- func/update.py
+++ func/update.py
@@ -7,33 +7,16 @@ window = tk.Tk()
 window.title("Update Pi-Ware")
 window.geometry("500x500")
 
-heading = tk.Label(window,
-                   text="""
-Update Pi-Ware
-""",
-                   font="Arial 15 bold")
+heading = tk.Label(window,text="""Update Pi-Ware""",font="Arial 15 bold")
 heading.pack()
 
 def update():
     os.system("lxterminal -e 'bash $HOME/pi-ware/updater'")
 
-update_button = tk.Button(window,
-          text="UPDATE",
-          font="Arial 11 bold",
-          bg="green",
-          fg="white",
-          width=100,
-          height=4,
-          command=update)
-
+update_button = tk.Button(window,text="UPDATE",font="Arial 11 bold",bg="green",fg="white",width=100,height=4,command=update)
 update_button.pack(side="bottom")
 
-description = tk.Label(
-    window,
-    text="""A new Pi-Ware update is available.
-Click the 'update' button to proceed.""",
-    font="Arial 12")
-
+description = tk.Label(window,text="""A new Pi-Ware update is available. Click the 'update' button to proceed.""",font="Arial 12")
 description.pack()
 
 window.mainloop()
