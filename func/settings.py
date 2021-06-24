2552fa75d0fb5f9b676d1b2b0bad70040764da67
oxmc <67136658+oxmc@users.noreply.github.com>
2021-06-24 02:45:21 -0700

    Update settings.py

--- func/settings.py
+++ func/settings.py
@@ -15,8 +15,10 @@ username = getpass.getuser()
 #Set window info
 window = tk.Tk()
 window.title("Pi-Ware Settings")
+
 #Set window image
 p1 = PhotoImage(file = f'/home/{username}/pi-ware/icons/logo.png')
+
 # Icon set for program window
 window.iconphoto(False, p1)
 window.geometry("500x500")
@@ -25,15 +27,29 @@ heading.pack()
 
 #Functions
 def change(setting, mode):
-    os.system("lxterminal -e 'bash $HOME/pi-ware/func/settings {setting} {mode}'")
+    os.system(f"lxterminal -e 'bash $HOME/pi-ware/func/settings {setting} {mode}'")
 
 #Buttons
-updatersettings_button = tk.Button(window,text="Change updater settings",font="Arial 11 bold",width=200,bg="darkblue",fg="white",command=change)
+se = next(os.walk(f"/home/{username}/pi-ware/func/settings/"))[1]
+settingslist = sorted(se)
+print("Current apps:\n")
+for app in settingslist:
+    print(app)
+    appb = ""
+    for a in app:
+        if(a == " "):
+            appb += "_"
+        else:
+            appb += a
+    exec(appb + f"""_button =  tk.Button(frame.inner, text=Change ${app} settings, font="Arial 11 bold", width=200, bg="darkblue", fg="white", command=change)""")
+    exec(appb + "_button.pack()")
+
+#updatersettings_button = tk.Button(window,text="Change updater settings",font="Arial 11 bold",width=200,bg="darkblue",fg="white",command=change)
 quit_button = tk.Button(window,text="Quit",font="Arial 11 bold",width=100,height=4,bg="green",fg="white",command=change)
 
 #Main
 description = tk.Label(window,text="""Change the settings of pi-ware.""",font="Arial 12")
 description.pack()
-updatersettings_button.pack()
+#updatersettings_button.pack()
 quit_button.pack(side="bottom")
 window.mainloop()
