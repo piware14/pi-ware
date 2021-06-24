0ba3bf04f0cc335dcc192cfdef672fdd9aaa7ce2
oxmc <67136658+oxmc@users.noreply.github.com>
2021-06-24 03:36:21 -0700

    Update settings.py

--- func/settings.py
+++ func/settings.py
@@ -29,6 +29,36 @@ heading.pack()
 def change(setting, mode):
     os.system(f"lxterminal -e 'bash $HOME/pi-ware/func/settings {setting} {mode}'")
 
+def show_desc(app):
+    global change, desc_win
+    desc_win = tk.Toplevel(window)
+    window.resizable(0, 0)
+    desc_win.title(f"{app}")
+    desc_win.geometry("320x500")
+    window.withdraw()
+    desc = open(f"/home/{username}/pi-ware/func/settings/{app}/setting.txt", "r")
+    desc_contents = desc.read()
+    app_desc = tk.Label(desc_win, text=desc_contents, font="Arial 9")
+    app_desc.pack()
+    install = tk.Button(desc_win, text="INSTALL", font="Arial 11 bold", width=200, bg="darkblue", fg="white", command=change(app, "off"))
+    install.pack()
+    back_to_menu_button = tk.Button(desc_win, text="BACK", font="Arial 11 bold", width=200, height=2, bg="green", fg="white", command=back_to_menu)
+    back_to_menu_button.pack(side = "bottom")
+    ucommand = f"bash '/home/{username}/pi-ware/func/uninst' %s" % app
+    command = f"bash '/home/{username}/pi-ware/func/inst' %s" % app
+    install_script = "'%s'" % command
+    uninstall_script = "'%s'" % ucommand
+
+def back_to_menu(window, parent, app=None):
+    parent.destroy()
+    window.deiconify()
+
+def back_to_menu():
+    window.deiconify()
+    desc_win.destroy()
+    window.title("Pi-Ware")
+    window.eval('tk::PlaceWindow . center')
+
 #Buttons
 se = next(os.walk(f"/home/{username}/pi-ware/func/settings/"))[1]
 settingslist = sorted(se)
