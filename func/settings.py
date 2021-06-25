cd2e1146098c22720b18d8f28c1922d42dd88fee
oxmc <67136658+oxmc@users.noreply.github.com>
2021-06-24 23:48:58 -0700

    Update settings.py

--- func/settings.py
+++ func/settings.py
@@ -59,7 +59,7 @@ window.iconphoto(False, p1)
 
 #Main
 window.resizable(0, 0)
-window.geometry("320x500")
+window.geometry("420x500")
 window.eval('tk::PlaceWindow . center')
 window.title("Pi-Ware Settings")
 #Show latest news message
