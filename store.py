647c5b7a232bb06c0339d6dd50221845f3cb58fe
Painadath <74921727+Painadath@users.noreply.github.com>
2021-06-26 11:25:12 +0530

    add icons to app buttons(Beta)

--- store.py
+++ store.py
@@ -6,46 +6,6 @@ import os
 from functools import partial
 import getpass
 
-class ScrolledFrame(tk.Frame):
-    def __init__(self, parent, vertical=True, horizontal=False):
-        super().__init__(parent)
-
-        # canvas for inner frame
-        self._canvas = tk.Canvas(self)
-        self._canvas.grid(row=0, column=0, sticky="news") # changed
-
-        # create right scrollbar and connect to canvas Y
-        self._vertical_bar = tk.Scrollbar(self, orient="vertical", command=self._canvas.yview)
-        if vertical:
-            self._vertical_bar.grid(row=0, column=1, sticky="ns")
-        self._canvas.configure(yscrollcommand=self._vertical_bar.set)
-
-        # create bottom scrollbar and connect to canvas X
-        self._horizontal_bar = tk.Scrollbar(self, orient="horizontal", command=self._canvas.xview)
-        if horizontal:
-            self._horizontal_bar.grid(row=1, column=0, sticky="we")
-        self._canvas.configure(xscrollcommand=self._horizontal_bar.set)
-
-        # inner frame for widgets
-        self.inner = tk.Frame(self._canvas, bg="red")
-        self._window = self._canvas.create_window((0, 0), window=self.inner, anchor="nw")
-
-        # autoresize inner frame
-        self.columnconfigure(0, weight=1) # changed
-        self.rowconfigure(0, weight=1) # changed
-
-        # resize when configure changed
-        self.inner.bind("<Configure>", self.resize)
-        self._canvas.bind("<Configure>", self.frame_width)
-
-    def frame_width(self, event):
-        # resize inner frame to canvas size
-        canvas_width = event.width
-        self._canvas.itemconfig(self._window, width = canvas_width)
-
-    def resize(self, event=None):
-        self._canvas.configure(scrollregion=self._canvas.bbox('all'))
-
 window = tk.Tk()
 #Set global var username
 global username
@@ -65,10 +25,9 @@ NewsMessagefile = open(f"/home/{username}/pi-ware/func/infomessage", "r")
 NewsMessagecontent = NewsMessagefile.read()
 NewsMessage = tk.Label(window, text=f"Latest news:\n{NewsMessagecontent}", font="Arial 11 bold")
 NewsMessage.pack()
-frame = ScrolledFrame(window)
-frame.pack(expand=True, fill="both")
-
-def show_desc(app):
+def show_desc(apt,*args):
+    item = tree.selection()[0]
+    app = tree.item(item,"text")
     global install_script, uninstall_script, desc_win
     desc_win = tk.Toplevel(window)
     p2 = PhotoImage(file = f'/home/{username}/pi-ware/apps/{app}/icon.png')
@@ -96,7 +55,11 @@ def show_desc(app):
 def back_to_menu(window, parent, app=None):
     parent.destroy()
     window.deiconify()
-
+tree = Treeview(window)
+tree.pack(expand=YES, fill=BOTH)
+tree.column("#0", minwidth=0, width=320, stretch=NO)
+s = Style()
+s.configure('Treeview', rowheight=35)
 ap = next(os.walk(f"/home/{username}/pi-ware/apps"))[1]
 applist = sorted(ap)
 print("Current apps:\n")
@@ -108,19 +71,19 @@ for app in applist:
             appb += "_"
         else:
             appb += a
-    exec(appb + """_button =  tk.Button(frame.inner, text=app, font="Arial 11 bold", width=200, bg="gray", fg="white", command=partial(show_desc,app))""")
-    exec(appb + "_button.pack()")
-
+    tree.bind("<<TreeviewSelect>>", partial(show_desc,app))
+    exec(appb + """_button =  PhotoImage(file=f'/home/{username}/pi-ware/apps/{app}/icon.png')""")
+    exec("""tree.insert('', 'end', text=f"{app}",image=""" + appb + """_button)""")
 def install_app():
     global install_script
     print(f"bash /home/{username}/pi-ware/func/term-run {install_script}")
     os.system(f"bash /home/{username}/pi-ware/func/term-run {install_script}")
-    
+
 def uninstall_app():
     global uninstall_script
     print(f"bash /home/{username}/pi-ware/func/term-run {uninstall_script}")
     os.system(f"bash /home/{username}/pi-ware/func/term-run {uninstall_script}")
-    
+
 def back_to_menu():
     window.deiconify()
     desc_win.destroy()
@@ -129,7 +92,7 @@ def back_to_menu():
 
 def quit():
     window.destroy()
-    
+
 quitbutton = tk.Button(window, text="Quit", font="Arial 11 bold", width=200, bg="grey", fg="white", command=quit)
 quitbutton.pack()
 
