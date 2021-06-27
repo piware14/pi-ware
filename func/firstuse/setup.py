c40600078a85b67a7fef4d84a4003ea98bb51f42
oxmc <67136658+oxmc@users.noreply.github.com>
2021-06-26 23:59:11 -0700

    Update setup.py

--- func/firstuse/setup.py
+++ func/firstuse/setup.py
@@ -7,7 +7,7 @@ window = tk.Tk()
 window.title("Setup Pi-Ware")
 window.geometry("500x500")
 
-heading = tk.Label(window,text="""Setup Pi-Ware""",font="Arial 15 bold")
+heading = tk.Label(window, text="""Setup Pi-Ware""", font="Arial 15 bold")
 heading.pack()
 
 def StoreData(Data, Location):
@@ -15,20 +15,20 @@ def StoreData(Data, Location):
     window.destroy()
 
 def retrieve_input():
-    inputValue=textBox.get("1.0","end-1c")
+    inputValue=textBox.get("1.0", "end-1c")
     print(inputValue)
     StoreData(inputValue, "$HOME/.local/share/pi-ware/passwd")
 
-Thankforinstall = tk.Label(window,text="""Thanks for installing pi-ware! \nLet's set some things up.""",font="Arial 12")
+Thankforinstall = tk.Label(window, text="""Thanks for installing pi-ware! \nLet's set some things up.""", font="Arial 12")
 Thankforinstall.pack()
 
-description = tk.Label(window,text="""First enter your password:""",font="Arial 12")
+description = tk.Label(window, text="""First enter your password:""", font="Arial 12")
 description.pack()
 
 textBox=tk.Text(window, height=2, width=10)
 textBox.pack()
 
-buttonCommit=tk.Button(root, height=1, width=10, text="Enter", command=lambda: retrieve_input())
+buttonCommit=tk.Button(window, height=1, width=10, text="Enter", command=lambda: retrieve_input())
 buttonCommit.pack()
 
 window.mainloop()
