39698170e5631c2c9973023c0b6e4a6806e78b7f
oxmc <67136658+oxmc@users.noreply.github.com>
2021-07-04 06:35:42 -0700

    Update store.py

--- store.py
+++ store.py
@@ -8,18 +8,9 @@ import webbrowser
 from functools import partial
 import getpass
 
-class HyperLink(tk.Label):
-	def __init__(self, parent, url, text=None, fg=None, cursor=None, *args, **kwargs):
-		self.url = url;
-		super().__init__(parent, text=(text or url),
-            fg=(fg or "blue"),
-            cursor=(cursor or "hand2"),
-            font="Arial 9",
-        *args, **kwargs)
-		self.bind("<Button-1>", self.web_open);
-
-	def web_open(self, event):
-		return webbrowser.open(self.url);
+#Import custom  pi-ware functions
+import pyfunc.classes
+import pyfunc.functions as func
 
 window = tk.Tk()
 #Set global var username
