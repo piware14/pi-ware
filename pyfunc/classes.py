c77c6f35f6708acbb32f82c4883bdc6dde1ffa06
oxmc <67136658+oxmc@users.noreply.github.com>
2021-07-04 06:33:11 -0700

    Create classes.py

---
+++ pyfunc/classes.py
@@ -0,0 +1,13 @@
+class HyperLink(tk.Label):
+	def __init__(self, parent, url, text=None, fg=None, cursor=None, *args, **kwargs):
+		self.url = url;
+		super().__init__(parent, text=(text or url),
+            fg=(fg or "blue"),
+            cursor=(cursor or "hand2"),
+            font="Arial 9",
+        *args, **kwargs)
+		self.bind("<Button-1>", self.web_open);
+
+	def web_open(self, event):
+		return webbrowser.open(self.url);
+  
