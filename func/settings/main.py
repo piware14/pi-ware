e2d345175ce44163ed76b762691bb83690a58208
oxmc <67136658+oxmc@users.noreply.github.com>
2021-07-11 04:09:47 -0700

    Update main.py

--- func/settings/main.py
+++ func/settings/main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Pi-Ware main UI
+# Pi-Ware Settings UI
 from tkinter import *
 from tkinter.ttk import *
 import tkinter as tk
@@ -12,9 +12,9 @@ import getpass
 global username
 username = getpass.getuser()
 
-#Set global install/uninstall scripts
-global install_script
-global uninstall_script
+#Set global inable/disable scripts
+global enable
+global disable
 
 #Import custom  pi-ware functions
 import classes
@@ -27,9 +27,9 @@ def show_desc(apt,*args):
     mainwiny = str(window.winfo_y())
     item = tree.selection()[0]
     app = tree.item(item,"text")
-    global install_script, uninstall_script, desc_win
+    global enable, disable, desc_win
     desc_win = tk.Toplevel(window)
-    p2 = PhotoImage(file = f'/home/{username}/pi-ware/apps/{app}/icon.png')
+    p2 = PhotoImage(file = f'/home/{username}/pi-ware/settings/options/{app}/icon.png')
     # Icon set for program window
     desc_win.iconphoto(False, p2)
     window.resizable(0, 0)
@@ -37,41 +37,17 @@ def show_desc(apt,*args):
     print("320x500+" + mainwinx + "+" + mainwiny)
     desc_win.geometry("320x500+" + mainwinx + "+" + mainwiny)
     window.withdraw()
-    desc = open(f"/home/{username}/pi-ware/apps/{app}/description.txt", "r")
+    desc = open(f"/home/{username}/pi-ware/settings/options/{app}/description", "r")
     desc_contents = desc.read()
     text_box = Text(desc_win, height=12, width=40)
     text_box.pack()
     text_box.insert('end', desc_contents)
     text_box.config(state='disabled')
-    #Disabled for now.
-    #app_desc = tk.Label(desc_win, text=desc_contents, font="Arial 9")
-    #app_desc.pack()
-    #Check if website file exist
-    filepath = f"/home/{username}/pi-ware/apps/{app}/website"
-    try:
-            file_tst = open(filepath)
-            file_tst.close()
 
-    except FileNotFoundError:
-            Web = "False"
-
-    else:
-            Web = "True"
-
-    #Add website from file
-    if Web == "True":
-            websiteurlfile = open(f'/home/{username}/pi-ware/apps/{app}/website', 'r')
-            websiteurl = websiteurlfile.readlines()
-            # Strips the newline character
-            for line in websiteurl:
-                #print("{}".format(line.strip()))
-                Website = classes.HyperLink(desc_win, f"""{line}""");
-                Website.pack()
-
-    install = tk.Button(desc_win, text="INSTALL", font="Arial 11 bold", width=200, bg="darkblue", fg="white", command=install_app)
-    install.pack()
-    uninstall = tk.Button(desc_win, text="UNINSTALL", font="Arial 11 bold", width=200, bg="red", fg="white", command=uninstall_app)
-    uninstall.pack()
+    enablebutton = tk.Button(desc_win, text="Enable", font="Arial 11 bold", width=200, bg="darkblue", fg="white", command=enable_setting)
+    enablebutton.pack()
+    disableutton = tk.Button(desc_win, text="Disable", font="Arial 11 bold", width=200, bg="red", fg="white", command=disable_setting)
+    disablebutton.pack()
     ucommand = f"""bash /home/{username}/pi-ware/func/term/uninst '{app}' 'Uninstalling {app}'"""
     command = f"""bash /home/{username}/pi-ware/func/term/inst '{app}' 'Installing {app}'"""
     install_script = "'%s'" % command
@@ -85,13 +61,13 @@ def back_to_menu(window, parent, app=None):
     window.deiconify()
 
 def install_app():
-    global install_script
+    global enable
     if IsDev == "True":
         print(f"bash /home/{username}/pi-ware/func/term/term-run {install_script}")
     os.system(f"bash /home/{username}/pi-ware/func/term/term-run {install_script}")
 
 def uninstall_app():
-    global uninstall_script
+    global disable
     if IsDev == "True":
         print(f"bash /home/{username}/pi-ware/func/term/term-run {uninstall_script}")
     os.system(f"bash /home/{username}/pi-ware/func/term/term-run {uninstall_script}")
@@ -99,7 +75,7 @@ def uninstall_app():
 def back_to_menu():
     window.deiconify()
     desc_win.destroy()
-    window.title("Pi-Ware")
+    window.title("Pi-Ware Settings")
     #window.eval('tk::PlaceWindow . center')
 
 def quit():
@@ -135,7 +111,6 @@ credits_tab = Frame(tab_control)
 DEV_tab = Frame(tab_control)
 tab_control.add(news_tab, text="News")
 tab_control.add(prefrence_tab, text="Prefrence")
-tab_control.add(credits_tab, text="Credits")
 #Show dev tab if dev files are found
 if IsDev == "True":
     tab_control.add(DEV_tab, text="Dev")
@@ -162,32 +137,14 @@ NewsMessagecontent = NewsMessagefile.read()
 NewsMessage = tk.Label(news_tab, text=f"Latest news:\n{NewsMessagecontent}", font="Arial 11 bold")
 NewsMessage.pack()
 
-#Show info message
-InfoMessagefile = open(f"/home/{username}/pi-ware/func/info/infomessage", "r")
-InfoMessagecontent = InfoMessagefile.read()
-InfoMessage = tk.Label(credits_tab, text=f"{InfoMessagecontent}", font="Arial 11 bold")
-InfoMessage.pack()
-
-#Show commit links
-commitmessage = tk.Label(credits_tab, text=f"To see commits, please go to the link below.", font="Arial 11 bold")
-commitmessage.pack()
-commit = classes.HyperLink(credits_tab, f"""https://github.com/piware14/pi-ware/graphs/contributors""");
-commit.pack()
-
-#Add pi-ware website
-piwarewebsite = tk.Label(credits_tab, text=f"To vist the pi-ware website, click the link below.", font="Arial 11 bold")
-piwarewebsite.pack()
-Website = classes.HyperLink(credits_tab, f"""https://pi-ware.ml""");
-Website.pack()
-
 tree = Treeview(prefrence_tab)
 tree.pack(expand=YES, fill=BOTH)
 tree.column("#0", minwidth=0, width=330, stretch=NO)
 s = Style()
 s.configure('Treeview', rowheight=35)
-ap = next(os.walk(f"/home/{username}/pi-ware/apps"))[1]
+ap = next(os.walk(f"/home/{username}/pi-ware/settings/options"))[1]
 applist = sorted(ap)
-print("Current apps:\n")
+print("Current settings:\n")
 for app in applist:
     print(app)
     appb = ""
@@ -197,8 +154,7 @@ for app in applist:
         else:
             appb += a
     tree.bind("<<TreeviewSelect>>", partial(show_desc,app))
-    exec(appb + """_button =  PhotoImage(file=f'/home/{username}/pi-ware/apps/{app}/icon.png')""")
-    exec("""tree.insert('', 'end', text=f"{app}",image=""" + appb + """_button)""")
+    exec("""tree.insert('', 'end', text=f"{app}"_button)""")
 
 ScrollForMore = tk.Label(prefrence_tab, text="Scroll down for more settings.", font="Arial 11 bold")
 ScrollForMore.pack()
