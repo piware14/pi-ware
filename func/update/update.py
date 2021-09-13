8c8efec994373e015266884b852b50cbd362d9c4
oxmc <67136658+oxmc@users.noreply.github.com>
2021-09-13 05:01:59 -0700

    Add files via upload

--- func/update/update.py
+++ func/update/update.py
@@ -3,21 +3,31 @@
 import tkinter as tk
 import os
 
+#Create window
 window = tk.Tk()
 window.title("Update Pi-Ware")
-window.geometry("500x500")
+window.geometry("510x500")
 
-heading = tk.Label(window,text="""Update Pi-Ware""",font="Arial 15 bold")
-heading.pack()
-
+#Functions
 def update():
     os.system("lxterminal -e 'bash $HOME/pi-ware/updater'")
     window.destroy()
 
-update_button = tk.Button(window,text="UPDATE",font="Arial 11 bold",bg="green",fg="white",width=100,height=4,command=update)
+#Heading
+heading = tk.Label(window,text="""Update Pi-Ware""",font="Arial 25 bold")
+
+#Descripton
+description1 = tk.Label(window,text="""A new Pi-Ware update is available.""",font="Arial 15")
+description2 = tk.Label(window,text="""Click the 'update' button to proceed.""",font="Arial 15")
+
+#Button
+update_button = tk.Button(window,text="UPDATE",font="Arial 15 bold",bg="green",fg="white",width=100,height=4,command=update)
+
+#Pack items
+heading.pack()
+description1.pack()
+description2.pack()
 update_button.pack(side="bottom")
 
-description = tk.Label(window,text="""A new Pi-Ware update is available. Click the 'update' button to proceed.""",font="Arial 12")
-description.pack()
-
 window.mainloop()
+
