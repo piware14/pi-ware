ed5e0e4a48e89d0187c2404eb775a9bbbda85a2e
oxmc <67136658+oxmc@users.noreply.github.com>
2021-12-08 21:39:27 -0800

    Update setup.py

--- func/firstuse/setup.py
+++ func/firstuse/setup.py
@@ -6,15 +6,23 @@ import tkinter as tk
 import os
 from functools import partial
 import getpass
+import datetime
 
 #Set global var username
 global username
 username = getpass.getuser()
 
+#Date calc
+current_time = datetime.datetime.now() 
+if current_time.month == "12":
+    if current_time.day == "25":
+        print("Merry Christmas from the pi-ware team!")
+
 #Create window
 window = tk.Tk()
 
 #Set window icon
+if date.month
 p1 = PhotoImage(file = f'/home/{username}/pi-ware/icons/logo.png')
 window.iconphoto(False, p1)
 
@@ -23,19 +31,15 @@ window.title("Setup Pi-Ware")
 window.geometry("800x500")
 window.eval('tk::PlaceWindow . center')
 
-
+#Functions
 def StoreData(Data, Location):
     os.system(f"echo '{Data}' > {Location}")
     window.destroy()
 
-def retrieve_input():
-    inputValue=textBox.get("1.0", "end-1c")
-
 #Define labels
 heading = tk.Label(window, text="""Setup Pi-Ware""", font="Arial 15 bold")
 Thankforinstall = tk.Label(window, text="""Thanks for installing pi-ware!\nLet's set some things up.""", font="Arial 12")
-description = tk.Label(window, text="""Do you want Pi-Ware to upload erros, logs and app info to the pi-ware team?\nPlease note, the pi-ware-team can not use this data to:\ntrack, monitor, or be used to identify you.""", font="Arial 12")
-
+description = tk.Label(window, text="""Do you want Pi-Ware to upload erros, logs and app info to the pi-ware team?\nPlease note, the pi-ware-team can not use this data to:\ntrack, monitor, or identify you.""", font="Arial 12")
 
 #Define buttons
 YesButton=tk.Button(window, height=1, width=10, text="Yes", command=lambda: StoreData("True", "$HOME/.local/share/pi-ware/telementry"))
