0e8f2de6bc428521e54a2d6cc96c27a97782f544
oxmc <67136658+oxmc@users.noreply.github.com>
2022-03-26 22:18:25 -0700

    Update main.py

--- store/main.py
+++ store/main.py
@@ -291,7 +291,7 @@ for app in applist:
       ficon = f'/home/{username}/pi-ware/apps/{app}/icon.png'
     else:
       ficon = f'/home/{username}/pi-ware/icons/app-no-icon.png'
-    print(appb + """_button =  PhotoImage(file='""" + ficon + """')""")
+    print(appb + """_button =  PhotoImage(file=f"{ficon}")""")
     exec(appb + """_button =  PhotoImage(file='""" + ficon + """')""")
     exec("""tree.insert('', 'end', text=f"{app}",image=""" + appb + """_button)""")
 
